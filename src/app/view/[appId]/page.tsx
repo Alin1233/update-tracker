49e4619ab6a0d24c3fa8828378fd6bf4ae6073f0
Alin1233 <herciualin10@gmail.com>
2024-04-08 09:42:26 +0200

    Update page.tsx

--- src/app/view/[appId]/page.tsx
+++ src/app/view/[appId]/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { PrismaClient } from '@prisma/client'
+import { AppType } from '@/schemas/AppType'
 
-const page = ({ params }) => {
-    return <p>App: {params.appId}</p>
+const page = async ({ params }) => {
+    const prisma = new PrismaClient()
+    const app = await prisma.apps.findUnique({
+        where: {
+            id: params.appId,
+        },
+    })
+    return <div>App: {app.name}</div>
 }
 
 export default page
